ecbac9887470306c46a1d0f5c500c1b4ce96168d
jungwone <prsjb77@gmail.com>
2024-05-15 16:45:25 +0900

    encrypt password using bcrypt and create new user in database

--- controller/auth.ts
+++ controller/auth.ts
@@ -1,7 +1,21 @@
+import bcrypt from "bcrypt";
 import { Request, Response } from "express";
+import * as userRepository from "../data/auth";
+import { config } from "../config";
 
 export async function singup(req: Request, res: Response) {
   const { email, password, name } = req.body;
 
-  res.status(201).json({ email, password, name });
+  const user = await userRepository.findUserByEmail(email);
+  if (user) {
+    return res
+      .status(409)
+      .json({ message: `${email}로 가입된 계정이 존재합니다` });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, config.bcrypt.saltRounds);
+
+  userRepository.createUser({ email, name, password: hashedPassword });
+
+  res.status(201).json({ message: "계정이 성공적으로 생성되었습니다." });
 }
